2d9e42569b79868735f033fe2a7dee1eb3a12311
Firxp011 <113378946+Firxp011@users.noreply.github.com>
2025-12-06 09:57:14 +0800

    add AndroidProducts.mk
    Signed-off-by: Firxp011 <113378946+Firxp011@users.noreply.github.com>

---
+++ AndroidProducts.mk
@@ -0,0 +1,12 @@
+#
+# Copyright (C) 2024 The Android Open Source Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/pb_peridot.mk
+
+COMMON_LUNCH_CHOICES := \
+    pb_peridot-user \
+    pb_peridot-userdebug \
+    pb_peridot-eng
